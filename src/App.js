a0de88006bf5ca5710e0cc00e2a043c39e74fd25
loretari <loretari@gmail.com>
2023-06-27 19:26:11 +0300

    init: CSS Transition using ReactTransitionGroup_1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -7,7 +8,8 @@ import List from "./components/List/List";
 
 class App extends Component {
     state = {
-        modalIsOpen: false
+        modalIsOpen: false,
+        showBlock: false
     }
 
     showModal = () => {
@@ -22,6 +24,23 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>React Animations</h1>
+                <button
+                    className="Button"
+                    onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))}
+                >
+                    Toggle
+                </button>
+                <br/>
+                {this.state.showBlock ? (
+                    <div
+                        style ={{
+                            backgroundColor: 'red',
+                    width: 100,
+                    height: 100,
+                            margin: 'auto'
+                    }}
+                   />
+                   ) : null}
                 {this.state.modalIsOpen ? <Modal show = {this.state.modalIsOpen} closed = {this.closeModal} /> : null}
                 {this.state.modalIsOpen ? <Backdrop show = {this.state.modalIsOpen} /> : null}
                 <button className="Button" onClick={this.showModal}>Open Modal</button>
